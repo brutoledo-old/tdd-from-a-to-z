f9c69295152092bb26dc5c981ad354c79cbae2c4
Bruno Toledo <bto@impact.dk>
2020-01-07 18:04:24 +0100

    Hangman.Core with TDD

---
+++ Hangman/Hangman.Core/Game.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Hangman.Core.Extensions;
+
+namespace Hangman.Core
+{
+    public class Game
+    {
+        public Game()
+        {
+            Topics = new Dictionary<string, List<string>>();
+            Topics.Add("Fruits", new List<string>() { "APPLE", "WATERMELON", "BANANA", "GRAPE", "BLUEBERRY" });
+            Topics.Add("Animals", new List<string>() { "TIGER", "CAT", "DOG", "WHALE", "LION" });
+            Topics.Add("Countries", new List<string>() { "DENMARK", "GERMANY", "BRAZIL", "PORTUGAL", "UNITED STATES" });
+        }
+
+        public Dictionary<string, List<string>> Topics { get; internal set; }
+
+        public string SecretWord { get; internal set; }
+
+        public double RemainingAttempts { get; set; }
+
+        public List<char> RemainingSecretLetters { get; internal set; }
+
+        public List<char> UsedLetters { get; set; }
+
+        public void SelectTopic(string topic)
+        {
+            if (!Topics.ContainsKey(topic))
+                throw new ArgumentException($"Could not find a topic with name: {topic}.");
+
+            SecretWord = Topics[topic].PickRandom();
+            RefreshGame();
+        }
+
+        private void RefreshGame()
+        {
+            RemainingSecretLetters = new List<char>(SecretWord);
+            RemainingAttempts = 6;
+            UsedLetters = new List<char>();
+        }
+
+        public void PickLetter(char c)
+        {
+            if (RemainingAttempts < 1)
+                throw new InvalidOperationException($"You cannot pick any more letter as you have no more remaining attempts");
+
+            if (RemainingSecretLetters.Count < 1)
+                throw new InvalidOperationException($"You cannot pick any more letter as there are no remaining letters");
+
+            UsedLetters.Add(c);
+
+            var upperChar = char.ToUpper(c);
+            if (SecretWord.Contains(upperChar))
+            {
+                RemainingSecretLetters.Remove(upperChar);
+            }
+            else
+            {
+                RemainingAttempts--;
+            }
+        }
+    }
+}
--- Hangman/Hangman.Tests/HangmanTests.cs
+++ Hangman/Hangman.Tests/HangmanTests.cs
@@ -1,3 +1,5 @@
+using System;
+using Hangman.Core;
 using NUnit.Framework;
 
 namespace Hangman.Tests
@@ -13,7 +15,7 @@ namespace Hangman.Tests
         //  |      / \
         //  |
         //vv|vvvvvvvvvvvvvvvvv
-        //  
+        //
         //  _   _   _   _   _
         //
         // Game Steps:
@@ -21,25 +23,18 @@ namespace Hangman.Tests
         // - Game randomly generates a word (from Topic) to be discovered
         // - Game exposes the number of letters (length)
         // - The player has 6 attempts to find the correct word otherwise the man is hanged in high
-        // 
+        //
         // Rules:
         // - The game must expose the already used letters (correct or wrong)
-
-        
-        [Test]
-        public void StartGame_GameIsInCorrectState()
-        {
-            Game game = new Game();
-            Assert.AreEqual(game.Topics.Count, 3);
-        }
 
         [Test]
         [TestCase("Fruits")]
         [TestCase("Animals")]
         [TestCase("Countries")]
-        public void StartGame_HasTheCorrectTopics(string topic)
+        public void StartGame_GameIsInCorrectState(string topic)
         {
             Game game = new Game();
+            Assert.AreEqual(3, game.Topics.Count);
             Assert.IsTrue(game.Topics.ContainsKey(topic));
         }
 
@@ -50,8 +45,132 @@ namespace Hangman.Tests
         public void StartGame_EachTopicHasAtLeast5DifferentWords(string topic)
         {
             Game game = new Game();
-            Assert.GreaterOrEqual(game.Topics[topic].Count, 5);
 
+            Assert.GreaterOrEqual(5, game.Topics[topic].Count);
         }
+
+        [Test]
+        [TestCase("Fruits")]
+        [TestCase("Animals")]
+        [TestCase("Countries")]
+        public void Game_RandomlyGeneratesTheTopicWord(string topic)
+        {
+            Game game = new Game();
+
+            game.SelectTopic(topic);
+
+            Assert.IsNotEmpty(game.SecretWord);
+            Assert.Contains(game.SecretWord, game.Topics[topic]);
+        }
+
+        [Test]
+        public void Game_SelectTopic_InvalidTopic_ThrowsException()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Game game = new Game();
+
+                game.SelectTopic("Cars");
+            });
+        }
+
+
+        [Test]
+        public void Game_SelectTopic_Refreshes_The_Game()
+        {
+            Game game = new Game();
+            
+            game.SelectTopic("Fruits");
+            game.PickLetter('Z');
+            Assert.Contains('Z', game.UsedLetters);
+
+            game.SelectTopic("Animals");
+            Assert.IsEmpty(game.UsedLetters);
+            Assert.AreEqual(6, game.RemainingAttempts);
+            Assert.AreEqual(game.SecretWord.Length, game.RemainingSecretLetters.Count);
+        }
+        
+        [Test]
+        public void Game_PickLetter_Updates_UsedLetters()
+        {
+            Game game = new Game();
+            game.SelectTopic("Fruits");
+            game.PickLetter('Z');
+
+            Assert.Contains('Z', game.UsedLetters);
+        }
+
+        [Test]
+        public void Game_PickLetter_Invalid_Letter()
+        {
+            Game game = new Game();
+            game.SelectTopic("Fruits");
+            game.PickLetter('Z');
+
+            Assert.AreEqual(5, game.RemainingAttempts);
+            Assert.Contains('Z', game.UsedLetters);
+        }
+
+        [Test]
+        public void Game_PickLetter_Case_Insentitive()
+        {
+            Game game = new Game();
+            game.SelectTopic("Fruits");
+
+            var capitalLetter = game.SecretWord[0];
+            game.PickLetter(Char.ToUpper(capitalLetter));
+            game.PickLetter(Char.ToLower(capitalLetter));
+
+            Assert.AreEqual(6, game.RemainingAttempts);
+            Assert.AreEqual(2, game.UsedLetters.Count);
+        }
+
+        [Test]
+        public void Game_PickLetter_Without_Remaining_Attempts()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                Game game = new Game();
+                game.SelectTopic("Fruits");
+                game.PickLetter('Z');
+                game.PickLetter('X');
+                game.PickLetter('Y');
+                game.PickLetter('V');
+                game.PickLetter('S');
+                game.PickLetter('C');
+                game.PickLetter('K');
+            });
+        }
+        
+        [Test]
+        public void Game_PickLetter_Valid_Letter()
+        {
+            Game game = new Game();
+            game.SelectTopic("Fruits");
+
+            var capitalLetter = game.SecretWord[0];
+            game.PickLetter(capitalLetter);
+
+            Assert.AreEqual(6, game.RemainingAttempts);
+            Assert.Less(game.RemainingSecretLetters.Count, game.SecretWord.Length);
+        }
+
+        [Test]
+        public void Game_PickLetter_Without_RemainingSecretLetters()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                Game game = new Game();
+                game.SelectTopic("Fruits");
+
+                for (int i = 0; i < game.SecretWord.Length; i++)
+                {
+                    game.PickLetter(game.SecretWord[i]);
+                }
+
+                game.PickLetter('A');
+            });
+        }
+
     }
 }
